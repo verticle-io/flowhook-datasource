6530aca4f46ae3845c3452d9c136bd8cafcd0bbf
Sam Jones <sam@verticle.io>
2020-10-28 21:35:55 +0100

    cleanup

--- src/module.ts
+++ src/module.ts
@@ -2,6 +2,5 @@ import { DataSourcePlugin } from '@grafana/data';
 import { DataSource } from './DataSource';
 import { ConfigEditor } from './ConfigEditor';
 import { QueryEditor } from './QueryEditor';
-//import { MyQuery, FlowhookDataSourceOptions } from './types';
 
 export const plugin = new DataSourcePlugin(DataSource).setConfigEditor(ConfigEditor).setQueryEditor(QueryEditor);
